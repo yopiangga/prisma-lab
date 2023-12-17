64aef982f09fae9459e8ad02aed7f3907d167176
yopiangga <yopigambyok@gmail.com>
2023-12-17 21:30:52 +0700

    change ip servre

--- src/config/Url.js
+++ src/config/Url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://192.168.1.227:5500";
-// export const baseUrl = "http://103.127.97.215:5500";
+// export const baseUrl = "http://192.168.1.227:5500";
+export const baseUrl = "http://147.139.171.126:5500";
